5482cb5b2f9b5c53881e284404c852ecf2da835a
crybigsea <crybigsea@outlook.com>
2019-07-30 14:49:16 +0800

    Spring IOC&DI&AOP

--- L14 - Spring AOP/demo/src/main/java/com/example/demo/config/ServiceConfig.java
+++ L14 - Spring AOP/demo/src/main/java/com/example/demo/config/ServiceConfig.java
@@ -1,22 +1,25 @@
 package com.example.demo.config;
 
-import com.example.demo.aspects.LoggerAspect;
-import com.example.demo.services.ProductService;
+// import com.example.demo.aspects.LoggerAspect;
+// import com.example.demo.services.ProductService;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScans({ @ComponentScan("com.example.demo.aspects"), @ComponentScan("com.example.demo.services") })
 public class ServiceConfig {
-    @Bean
-    public ProductService productService() {
-        return new ProductService();
-    }
+    // @Bean
+    // public ProductService productService() {
+    // return new ProductService();
+    // }
 
-    @Bean
-    public LoggerAspect loggerAspect(){
-        return new LoggerAspect();
-    }
+    // @Bean
+    // public LoggerAspect loggerAspect(){
+    // return new LoggerAspect();
+    // }
 }
--- L14 - Spring AOP/demo/src/main/java/com/example/demo/services/ProductService.java
+++ L14 - Spring AOP/demo/src/main/java/com/example/demo/services/ProductService.java
@@ -1,5 +1,8 @@
 package com.example.demo.services;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductService {
     public void GetProductList() {
         System.out.println("*****product list******");
